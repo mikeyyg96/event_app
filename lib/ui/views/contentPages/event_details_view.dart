9984c78fa5394c75a3bdfef68d5346e706e3bba6
blake_reeves <blake.reeves@oldhickorybuildings.com>
2020-02-04 17:01:15 -0600

    Further Event Details Changes

--- lib/ui/views/contentPages/event_details_view.dart
+++ lib/ui/views/contentPages/event_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:event_app/core/viewmodels/contentPages/event_details_model.dart';
 import 'package:flutter/material.dart';
 import 'package:event_app/core/classes/event.dart';
+import 'package:event_app/ui/shared/styling.dart';
 
 import '../base_view.dart';
 
@@ -16,86 +17,155 @@ class EventDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseView<EventDetailsModel>(
       builder: (context, model, child) => Scaffold(
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height / 2.5,
-                child: Stack(
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 2.9,
+              child: Stack(
+                children: <Widget>[
+                  Hero(
+                    tag: event.category,
+                    child: Image.asset(event.image),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 24.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        iconSize: 32.0,
+                        icon: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 24.0),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        onPressed: () => print('Favorite Event'),
+                        iconSize: 32.0,
+                        icon: CircleAvatar(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          child: Icon(
+                            Icons.favorite,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 24.0),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white70,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(8.0),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '\$${event.price.toString()}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: Column(
                   children: <Widget>[
-                    Hero(
-                      tag: event.category,
-                      child: Image.asset(event.image),
-                    ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 24.0),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: IconButton(
-                          onPressed: () => Navigator.pop(context),
-                          iconSize: 32.0,
-                          icon: CircleAvatar(
-                            backgroundColor: Colors.white,
-                            child: Icon(
-                              Icons.arrow_back,
-                              color: Theme.of(context).primaryColor,
-                            ),
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            event.name,
+                            style: stylingHeader,
                           ),
-                        ),
+                        ],
                       ),
                     ),
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    //   child: Row(
+                    //     crossAxisAlignment: CrossAxisAlignment.start,
+                    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //     children: <Widget>[
+                    //       Text('10'),
+                    //       Container(
+                    //         width: 1,
+                    //         height: MediaQuery.of(context).size.height / 48,
+                    //         color: Colors.grey,
+                    //         child: VerticalDivider(),
+                    //       ),
+                    //       Text('10'),
+                    //       Text('10'),
+                    //     ],
+                    //   ),
+                    // ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 24.0),
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: IconButton(
-                          onPressed: () => print('Favorite Event'),
-                          iconSize: 32.0,
-                          icon: CircleAvatar(
-                            backgroundColor: Theme.of(context).primaryColor,
-                            child: Icon(
-                              Icons.favorite,
-                              color: Colors.white,
-                            ),
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Event Info',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
                           ),
-                        ),
+                        ],
                       ),
                     ),
+                    Expanded(
+                      child: Container(color: Colors.red,),
+                    ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 64.0),
-                      child: Align(
-                        alignment: Alignment.bottomRight,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white70,
-                            shape: BoxShape.rectangle,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(8.0),
-                            ),
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          RaisedButton(
+                            onPressed: () => print('test'),
+                            child: Text('Test'),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              '\$${event.price.toString()}',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
+                          RaisedButton(
+                            onPressed: () => print('test'),
+                            child: Text('Test'),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-              Expanded(
-                child: Container(
-                  color: Colors.white,
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
